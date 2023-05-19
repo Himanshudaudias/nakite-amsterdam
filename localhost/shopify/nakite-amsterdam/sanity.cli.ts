3c7d1f8341b80b458454399ad5865ea7ebe7a840
kishan <kishan@cmsminds.com>
2023-05-19 12:33:05 +0530

    kk update

---
+++ localhost/shopify/nakite-amsterdam/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'wgib3qjy',
+    dataset: 'production'
+  }
+})
---
+++ localhost/shopify/nakite-amsterdam/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Shopify - nakite-amsterdam',
+
+  projectId: 'wgib3qjy',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
