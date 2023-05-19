3c39d559cb52afb17a5c2a990c85ef877d043eee
kishan <kishan@cmsminds.com>
2023-05-19 12:39:02 +0530

    kk update

--- app/components/FullPageScroll.jsx
+++ app/components/FullPageScroll.jsx
@@ -10,30 +10,69 @@ import firstblockimg from '../images/lastslide-two.png'
 import thirdblockimg from '../images/lastslidebottom.png'
 
 
-export function Layout({children, title}) {
-  return (
-    <div className="flex flex-col min-h-screen antialiased bg-neutral-50">
-      <header
-        role="banner"
-        className={`flex items-center h-16 p-6 md:p-8 lg:p-12 sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 antialiased transition shadow-sm`}
-      >
-        <div className="flex gap-12">
-          <a className="font-bold" href="/">
-            {title}
-          </a>
-        </div>
-      </header>
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from '@remix-run/react';
+import styles from './styles/app.css';
+import favicon from '../public/favicon.svg';
 
-      <main
-        role="main"
-        id="mainContent"
-        className="flex-grow p-6 md:p-8 lg:p-12"
-      >
-        {children}
-      </main>
-    </div>
+export const links = () => {
+  return [
+    {rel: 'stylesheet', href: styles},
+    {
+      rel: 'preconnect',
+      href: 'https://cdn.shopify.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://shop.app',
+    },
+    {rel: 'icon', type: 'image/svg+xml', href: favicon},
+  ];
+};
+
+export async function loader({context}) {
+  const layout = await context.storefront.query(LAYOUT_QUERY);
+  return {layout};
+}
+
+export default function App() {
+  const data = useLoaderData();
+
+  const {name} = data.layout.shop;
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>Hello, {name}</h1>
+        <p>This is a custom storefront powered by Hydrogen</p>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
   );
 }
+
+const LAYOUT_QUERY = `#graphql
+  query layout {
+    shop {
+      name
+      description
+    }
+  }
+`;
 
 export function FullPageScroll() {
     return (
